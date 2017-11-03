96f060e57e37155829745c2b217c5a5eb494b444
Darain <d29702@163.com>
2017-11-03 17:14:29 +0800

    add ai files

---
+++ src/ai/mytf/mytf.py
@@ -0,0 +1,26 @@
+import numpy as np
+import tensorflow as tf
+import random
+
+# reproducible
+np.random.seed(1)
+tf.set_random_seed(1)
+
+class  PG1():
+    def __init__(self,actions,learningRate=0.002,rewardDecay=0.999):
+        self.actions=actions
+        self.lr=learningRate
+        self.sess = tf.Session()
+
+
+        self.init=tf.global_variables_initializer()
+        self.sess.run(self.init)
+
+
+    def _buildNet(self):
+        layers=[]
+        layers.append(
+            tf.layer.dense(
+                
+            )
+        )
---
+++ src/ai/tfpreflop/mypg.py
@@ -0,0 +1,47 @@
+from RL_brain import PolicyGradient
+import matplotlib.pyplot as plt
+import numpy as np
+
+actions=['fold','call','raise']
+RL = PolicyGradient(
+    n_actions=len(actions)
+    n_features=len(actions)
+    learning_rate=0.02,
+    reward_decay=0.995,
+    # output_graph=True,
+)
+
+for i_episode in range(1000):
+
+    
+
+    while True:
+
+        action = RL.choose_action(observation)
+
+        observation_, reward, done, info = env.step(action)     # reward = -1 in all cases
+
+        RL.store_transition(observation, action, reward)
+
+        if done:
+            # calculate running reward
+            ep_rs_sum = sum(RL.ep_rs)
+            if 'running_reward' not in globals():
+                running_reward = ep_rs_sum
+            else:
+                running_reward = running_reward * 0.99 + ep_rs_sum * 0.01
+            if running_reward > DISPLAY_REWARD_THRESHOLD: RENDER = True     # rendering
+
+            print("episode:", i_episode, "  reward:", int(running_reward))
+
+            vt = RL.learn()  # train
+
+            if i_episode == 30:
+                plt.plot(vt)  # plot the episode vt
+                plt.xlabel('episode steps')
+                plt.ylabel('normalized state-action value')
+                plt.show()
+
+            break
+
+        observation = observation_
