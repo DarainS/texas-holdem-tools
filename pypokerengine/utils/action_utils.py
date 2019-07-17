588d45fbb2abc663684dba538d0a1b41972aa800
Shougang Deng <shougang.deng@shopee.com>
2019-07-17 18:32:39 +0800

    format fix

--- pypokerengine/utils/action_utils.py
+++ pypokerengine/utils/action_utils.py
@@ -4,10 +4,11 @@ ACTION_CALL = "call"
 ACTION_FOLD = "fold"
 ACTION_RAISE = "raise"
 
+
 def generate_legal_actions(players, player_position, sb_amount):
-    return ActionChecker.legal_actions(players, player_position, sb_amount)
+	return ActionChecker.legal_actions(players, player_position, sb_amount)
+
 
 def is_legal_action(players, player_position, sb_amount, action, amount=None):
-    return ActionChecker._is_legal(
-            players, player_position, sb_amount, action, amount)
-
+	return ActionChecker._is_legal(
+		players, player_position, sb_amount, action, amount)
